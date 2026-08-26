f6f6e014c416056ef4da7b86c05017886d180d59
abdullahyaseen1121 <abdullahyaseen1121@gmail.com>
2026-08-26 13:04:01 +0500

    ad new file

---
+++ app.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Welcome to my React App.</p>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
